0a5902c7f238c9665ce9520a085e00e676f26397
jpartee <jparteek@walmartlabs.com>
2013-05-04 00:14:20 -0700

    defined schema

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
+
+
+/*
+* More details here http://mongoosejs.com/docs/guide.html
+*/
+
+var mongoose = require("mongoose");
+
+//connect to database
+var db = mongoose.createConnection('192.168.16.128');
+
+//create schema for blog post
+var blogSchema = new mongoose.Schema({
+  title:  String,
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
+//compile schema to model
+var Blog = db.model('blog', blogSchema)
+console.log("Created model");
+
+
+
+
 /*
 * More details here http://mongoosejs.com/docs/index.html
-*/
+
 
 //require mongoose node module
 var mongoose = require('mongoose');
@@ -14,7 +46,7 @@ db.on('connected', function() {
 });
 
 
-
+*/
 
 
 
